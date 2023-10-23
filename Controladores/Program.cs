79242237e54de3d8090e448a5ffedb27d9f835de
DESKTOP-C638FBT\csi23-salflet <csi23-salflet@colegioaltair.net>
2023-10-23 14:17:55 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,56 @@
+﻿using ParImpar.Servicios;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+
+namespace ParImpar;
+
+class Program 
+{
+    static void Main(string[] args)
+    {   
+        
+        int n1;
+        bool cerrar = false;
+        int opcionSeleccionada;
+
+        while (!cerrar)
+        { 
+            MenuInterfaz mi = new MenuImplementacion();
+         
+            int selec = mi.Menu();
+      
+            switch (selec)
+            {
+                case 0:
+                    Console.WriteLine("[INFO] - se ejecuta caso uno 0");
+                    cerrar = true;
+                    break;
+
+                case 1:
+                    Console.WriteLine("Ingresa un número");
+                    n1 = Convert.ToInt32(Console.ReadLine());
+
+
+                    if ((n1 % 2) == 0)
+                    {
+                        Console.WriteLine("Es par");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Es impar");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("");
+                    break;
+
+            }
+
+        }
+
+    }
+
+
+
+}
---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public int Menu()
+        {
+            int selec;
+
+            Console.WriteLine("###########################");
+            Console.WriteLine("0. Se cerrara la aplicacion");
+            Console.WriteLine("1. Introducir numero y ver si es par o impar");
+            Console.WriteLine("###########################");
+            selec = Convert.ToInt32(Console.ReadLine());
+
+            return selec;
+        }
+    }
+}
